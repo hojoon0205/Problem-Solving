66186526af31ce2ee957f2b3e009af7dd52208ce
hojoon0205 <hojoon0205@gmail.com>
2020-04-21 01:56:59 +0900

    1 problem for PS, still upsolving on GCJ R1B

---
+++ Competition/GoogleCodeJam/2020/R1B/b.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+ll A, B;
+
+int main()
+{
+    int T, tt;
+    scanf("%d%lld%lld", &T, &A, &B);
+    for(tt=1; tt<=T; ++tt){
+        
+    }
+}
